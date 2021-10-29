4fa660c4ff5701f366fe53e0479a1cd3215fd8be
Noopur Chhajed <noopurchhajed98@gmail.com>
2021-10-29 17:29:32 +0530

    Final receiver rendered.

--- src/components/rewards/steps/ConfirmTransfer.js
+++ src/components/rewards/steps/ConfirmTransfer.js
@@ -1,48 +1,87 @@
-import React,{ useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import {  reduxForm } from 'redux-form';
+import { reduxForm } from 'redux-form';
 import { Card } from '../../shared/Footer.styled'
 import Button from 'components/shared/Button.styled'
 import Footer from 'components/shared/Footer'
-import { getParseHtmlArticle } from 'utils/helpers'
+import { getCountryName, getParseHtmlArticle } from 'utils/helpers'
 import { GET_STEP_PROGRESSBAR } from 'constants/app'
 import PropTypes from 'prop-types';
 import { transactionDetailsValidate as validate } from 'utils/validates'
 import SenderDetails from 'components/transactionDetails/transactionHistory/senderDetails'
+import { postDeliveryData } from 'middleware/receiver'
+import BorderTitle from 'components/shared/BorderTitle.styled';
+
 const ConfirmTransfer = (props) => {
-    const { handleSubmit , prevPage } = props;
+    const { handleSubmit, prevPage } = props;
     const dispatch = useDispatch()
     const formValues = useSelector((state) => state.form.receiver_details)
     const incomeDetail = useSelector((state) => state.user.incomeDetail)
-    const postDeliveryData = useSelector((state) => state.receiver.postDeliveryData)
+    const postDeliveryDetails = useSelector((state) => state.receiver.postDeliveryData)
+    const countries = useSelector((state) => state.receiver.countries )
 
-    useEffect(() =>{
+    useEffect(() => {
         dispatch({
             type: GET_STEP_PROGRESSBAR,
             data: { title: 'Confirm Transfer', step: 4 }
         })
-        console.log(formValues,postDeliveryData)
-    },[])
+        console.log(formValues, postDeliveryDetails)
+    }, [])
 
     const saveData = (values) => {
-        dispatch(postDeliveryData(values,incomeDetail))
+        dispatch(postDeliveryData(values, incomeDetail))
+    }
+
+    const payoutLocationText =  () => {
+        if(postDeliveryDetails?.receiver?.address.country_iso_code !== 'US') {
+            if(postDeliveryDetails?.paymentDetails?.fix_on_send === 'N') {
+                return 'Expected Foreign Country Payout Location'
+            } else {
+                return 'Foreign Country Payout Location'
+            }
+        } else if(postDeliveryDetails?.paymentDetails?.fix_on_send === 'N') {
+            return 'Expected Payout Location'
+        } else {
+            return 'Payout Location'
+        }
     }
 
     return (
         <Card>
 
             <div>
-                <h5>Select a Delivery Type</h5>
                 <form onSubmit={ handleSubmit(saveData) } >
                     <h5>NOT A RECEIPT</h5>
                     <p>Date of transaction: </p>
                     {getParseHtmlArticle('en_wu_118')}
-                    {postDeliveryData?.sender && <SenderDetails sender={ postDeliveryData?.sender } />}
+                    {postDeliveryDetails?.sender && <SenderDetails sender={ postDeliveryDetails?.sender } />}
+                    <BorderTitle smallText className="mt-4">Final Receiver</BorderTitle>
 
-                    <Footer>
-                        <Button onClick={ prevPage } >Back</Button>
-                        <Button outlined type='submit'>Continue</Button>
-                    </Footer>
+                    <div className="d-flex justify-content-between info">
+                        <p>Name</p>
+                        <span><b> {postDeliveryDetails?.receiver?.name?.first_name || ''} {postDeliveryDetails?.receiver?.name?.middle_name || ''} {postDeliveryDetails?.receiver?.name.last_name || ''}</b>
+                        </span>
+                    </div>
+
+                    <div className="d-flex justify-content-between info">
+                        <p>{payoutLocationText()}</p>
+                        <span><b>{getCountryName(countries, postDeliveryDetails?.receiver?.address.country_iso_code)}</b>
+                        </span>
+                    </div>
+                    {postDeliveryDetails?.receiver?.address.city &&
+                        <div className="d-flex justify-content-between info">
+                            <p>Payout City</p>
+                            <span><b>{postDeliveryDetails?.receiver?.address.city}</b>
+                            </span>
+                        </div>
+                    }
+                    {getParseHtmlArticle('en_wu_117')}
+                    {getParseHtmlArticle('en_wu_114')}
+                    <Button onClick={ prevPage } >Edit details</Button>
+                    <Button outlined type='submit'>Confirm and Send</Button>
+                    {getParseHtmlArticle('en_wu_115')}
+                    {getParseHtmlArticle('en_wu_111')}
+                    <Footer/>
                 </form>
             </div>
         </Card>
