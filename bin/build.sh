ab554e40b75b24ca2d33633ea3caa90fcf61909e
Gagan Bahety <gbahety@cimagroup.local>
2021-11-09 01:27:17 +0530

    added bin directory for build and cloudfront invalidation

---
+++ bin/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export AWS_PROFILE=jenkins_cuentas
+
+if [ $# -lt 1 ]; then
+  echo "USAGE:  build.sh env dist_id "
+  exit 1
+fi
+
+env=$1
+dist_id=$2
+
+source ~/.bashrc
+
+nvm use 10
+
+yarn install && yarn run build
+
+DIST_ID=${dist_id}  node bin/invalidate.js
