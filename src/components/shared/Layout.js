31eadf662442da23a2e148be7ac1490bad8a4379
Noopur Chhajed <noopurchhajed98@gmail.com>
2021-11-11 14:31:37 +0530

    Fixed cancel transaction issue.

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types';
 import { ROUTES } from 'constants/AppRoutes'
 import Navbar from './Navbar'
 import MainLoader from './Loader'
-// import { getAllArticles } from 'middleware/articles';
-// import { useDispatch, useSelector } from 'react-redux'
 import {  useSelector } from 'react-redux'
 import history from 'utils/history'
 import StyledContainer from '../shared/Container.styled'
-// import _ from 'lodash'
+
 export const Layout = (props) => {
-    // const dispatch = useDispatch()
-    // const articles = useSelector((state) => state.articles.articles)
+
     const showProgressBar = () => {
         return [ ROUTES.RECEIVER_DETAILS ].includes(history.location.pathname)
     }
@@ -31,7 +28,8 @@ export const Layout = (props) => {
         state.user.loading ||
         state.transactionHistory.loading ||
         state.transactionHistory.enquiryLoading ||
-        state.transactionHistory.confirmLoading
+        state.transactionHistory.confirmLoading ||
+        state.transactionHistory.cancelLoading
     )
 
     useEffect(async() => {
