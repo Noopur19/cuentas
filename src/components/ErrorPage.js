7d8102d624460b2ba5addf8a1df98f08294f704b
Noopur Chhajed <noopurchhajed98@gmail.com>
2021-11-12 18:05:09 +0530

    Error Popoup

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import Modal from 'components/shared/Modal';
+import { useTranslation } from 'react-i18next';
+
 const ErrorPage = () => {
-    return(<>
-        Session has been Expired Or Invalid token
-    </>)
+    const { t } = useTranslation()
+    const [ isOpen, setIsOpen ] = useState(true);
+
+    const toggleModal = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return(
+        <Modal
+            show={ isOpen }
+            handleCancel={ () => toggleModal() }
+            rightButtonText={ t('CLOSE_TEXT') }
+        >
+            <h3>{t('OOPS_ERROR')}</h3>
+            <h4>{t('VISIT_APP_STORE')}</h4>
+        </Modal>
+    )
 }
 
 export default ErrorPage
