7e80072f578e58a4a2899add2b3ed17f9ba0919b
Noopur Chhajed <noopurchhajed98@gmail.com>
2021-11-18 17:12:11 +0530

    Login commented

--- src/middleware/login.js
+++ src/middleware/login.js
@@ -8,20 +8,20 @@ import {
 } from 'actions/login';
 import { getStoreDetails, getUserDetails } from './user';
 import history from 'utils/history'
-import axiosInstance from 'services/api';
-const querystring = require('querystring')
+// import axiosInstance from 'services/api';
+// const querystring = require('querystring')
 export const login = (access_token, t) => {
     return async(dispatch) => {
         dispatch(loginRequest())
         if(access_token){
-            const result = await axiosInstance.post('/oauth/token', querystring.stringify({
-                username: 'tk2041@mailinator.com',
-                password: 'Secure@123',
-                grant_type: 'password',
-                client_id: 'knetik'
-            }))
-            setLocalData('accessToken', result.data.access_token)
-            //setLocalData('accessToken', access_token)
+            // const result = await axiosInstance.post('/oauth/token', querystring.stringify({
+            //     username: 'tk2041@mailinator.com',
+            //     password: 'Secure@123',
+            //     grant_type: 'password',
+            //     client_id: 'knetik'
+            // }))
+            // setLocalData('accessToken', result.data.access_token)
+            setLocalData('accessToken', access_token)
             await dispatch(getUserDetails(t))
             await dispatch(getStoreDetails())
             dispatch(loginSuccess(access_token))
